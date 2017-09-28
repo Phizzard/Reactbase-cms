05b514930a9f07efef4e9b383b65ae36f8fe36df
Philip Tietjen <tietjen.philip@gmail.com>
2017-09-27 23:26:15 -0400

    Add Number and checkbox inputs

--- src/components/Content/ContextualInput.js
+++ src/components/Content/ContextualInput.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Checkbox from 'react-checkbox';
 
 export default class ContextualInput extends Component {
     constructor(props){
@@ -11,6 +12,7 @@ export default class ContextualInput extends Component {
         }
 
         this.handleInput = this.handleInput.bind(this);
+        this.handleToggle = this.handleToggle.bind(this);
     }
     componentDidMount(){
         this.setState({
@@ -41,6 +43,21 @@ export default class ContextualInput extends Component {
                     </div>
                 );
                 break;
+            case 'number':
+                return(
+                    <div className={`form-group ${this.props.isTemplate && 'isTemplateInput'}`}>
+                        <label htmlFor={this.props.id}>{this.props.label}</label>
+                        <input  type="number"
+                                step="any"
+                                className="form-control"
+                                id={this.props.id}
+                                value={this.state.value}
+                                onChange={this.handleInput}
+                                disabled={this.props.isTemplate}
+                        />
+                    </div>
+                );
+                break;
             case 'currency':
                 return(
                     <div className={`form-group ${this.props.isTemplate && 'isTemplateInput'}`}>
@@ -56,6 +73,14 @@ export default class ContextualInput extends Component {
                                     disabled={this.props.isTemplate}
                             />
                         </div>
+                    </div>
+                );
+                break;
+            case 'checkbox':
+                return(
+                    <div className={`form-group ${this.props.isTemplate && 'isTemplateInput'}`}>
+                        <p>{this.props.label}</p>
+                        <button id={this.props.id} className={`btn btn-small ${ this.state.value ? 'btn-success' : 'btn-danger'}`} onClick={this.handleToggle}>{this.state.value.toString()}</button>
                     </div>
                 );
                 break;
@@ -80,6 +105,14 @@ export default class ContextualInput extends Component {
         this.setState({
             value: element.value
         }, this.props.updateRecordFormState({[element.id]: {value: element.value}}));
+    }
+    handleToggle(e){
+        e.preventDefault();
+        let element = e.target;
+        console.log(this.state.value);
+        this.setState({
+            value: !this.state.value
+        }, this.props.updateRecordFormState(({[element.id]: {value: !this.state.value}})));
     }
 }
 
